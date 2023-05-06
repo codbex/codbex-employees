d5959b77fbd696cba37772760461c70763301281
delchev <delchevn@gmail.com>
2023-05-06 17:03:01 +0300

    initial

---
+++ codbex-employees-app/gen/ui/Entities/Employee/controller.js
@@ -0,0 +1,128 @@
+angular.module('page', ["ideUI", "ideView", "entityApi"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'codbex-employees-app.Entities.Employee';
+	}])
+	.config(["entityApiProvider", function (entityApiProvider) {
+		entityApiProvider.baseUrl = "/services/js/codbex-employees-app/gen/api/Entities/Employee.js";
+	}])
+	.controller('PageController', ['$scope', '$http', 'messageHub', 'entityApi', function ($scope, $http, messageHub, entityApi) {
+
+		function resetPagination() {
+			$scope.dataPage = 1;
+			$scope.dataCount = 0;
+			$scope.dataLimit = 20;
+		}
+		resetPagination();
+
+		//-----------------Events-------------------//
+		messageHub.onDidReceiveMessage("entityCreated", function (msg) {
+			$scope.loadPage($scope.dataPage);
+		});
+
+		messageHub.onDidReceiveMessage("entityUpdated", function (msg) {
+			$scope.loadPage($scope.dataPage);
+		});
+		//-----------------Events-------------------//
+
+		$scope.loadPage = function (pageNumber) {
+			$scope.dataPage = pageNumber;
+			entityApi.count().then(function (response) {
+				if (response.status != 200) {
+					messageHub.showAlertError("Employee", `Unable to count Employee: '${response.message}'`);
+					return;
+				}
+				$scope.dataCount = response.data;
+				let offset = (pageNumber - 1) * $scope.dataLimit;
+				let limit = $scope.dataLimit;
+				entityApi.list(offset, limit).then(function (response) {
+					if (response.status != 200) {
+						messageHub.showAlertError("Employee", `Unable to list Employee: '${response.message}'`);
+						return;
+					}
+					$scope.data = response.data;
+				});
+			});
+		};
+		$scope.loadPage($scope.dataPage);
+
+		$scope.selectEntity = function (entity) {
+			$scope.selectedEntity = entity;
+		};
+
+		$scope.openDetails = function (entity) {
+			$scope.selectedEntity = entity;
+			messageHub.showDialogWindow("Employee-details", {
+				action: "select",
+				entity: entity,
+				optionsOrganisatoinId: $scope.optionsOrganisatoinId,
+			});
+		};
+
+		$scope.createEntity = function () {
+			$scope.selectedEntity = null;
+			messageHub.showDialogWindow("Employee-details", {
+				action: "create",
+				entity: {},
+				optionsOrganisatoinId: $scope.optionsOrganisatoinId,
+			}, null, false);
+		};
+
+		$scope.updateEntity = function (entity) {
+			messageHub.showDialogWindow("Employee-details", {
+				action: "update",
+				entity: entity,
+				optionsOrganisatoinId: $scope.optionsOrganisatoinId,
+			}, null, false);
+		};
+
+		$scope.deleteEntity = function (entity) {
+			let id = entity.Id;
+			messageHub.showDialogAsync(
+				'Delete Employee?',
+				`Are you sure you want to delete Employee? This action cannot be undone.`,
+				[{
+					id: "delete-btn-yes",
+					type: "emphasized",
+					label: "Yes",
+				},
+				{
+					id: "delete-btn-no",
+					type: "normal",
+					label: "No",
+				}],
+			).then(function (msg) {
+				if (msg.data === "delete-btn-yes") {
+					entityApi.delete(id).then(function (response) {
+						if (response.status != 204) {
+							messageHub.showAlertError("Employee", `Unable to delete Employee: '${response.message}'`);
+							return;
+						}
+						$scope.loadPage($scope.dataPage);
+						messageHub.postMessage("clearDetails");
+					});
+				}
+			});
+		};
+
+		//----------------Dropdowns-----------------//
+		$scope.optionsOrganisatoinId = [];
+
+		$http.get("/services/js/codbex-employees-app/gen/api/Entities/Organisatoin.js").then(function (response) {
+			$scope.optionsOrganisatoinId = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+		$scope.optionsOrganisatoinIdValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsOrganisatoinId.length; i++) {
+				if ($scope.optionsOrganisatoinId[i].value === optionKey) {
+					return $scope.optionsOrganisatoinId[i].text;
+				}
+			}
+			return null;
+		};
+		//----------------Dropdowns-----------------//
+
+	}]);
